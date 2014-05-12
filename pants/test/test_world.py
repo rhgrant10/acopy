eb81d2ba1776ad4d9a488f197434f4b3e1099a15
Robert Grant <rgrant@mdi.us.com>
2014-05-12 13:57:14 -0400

    Added unit test for euclidean_edges

--- pants/test/test_world.py
+++ pants/test/test_world.py
@@ -1,5 +1,6 @@
 from ..world import World, Edge
 import unittest
+import math
 
 class WorldTest(unittest.TestCase):
     known_distances = [
@@ -14,11 +15,33 @@ class WorldTest(unittest.TestCase):
         ((( 0,  0), ( 3,  4)), 5),
     ]
     
+    coords = [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (1, 0),
+    ]
+    
+    euclidean_answers = [
+        (0, 1, 1),
+        (0, 2, 2),
+        (0, 3, 1),
+        (1, 2, 1),
+        (1, 3, math.sqrt(2)),
+        (2, 3, math.sqrt(5))
+    ]
+        
     def test_edge_distance(self):
         for (a, b), d in self.known_distances:
             e = Edge(a, b)
             self.assertEqual(e.distance, d)
-            
+    
+    def test_world_euclidean_edges(self):
+        edges = World.euclidean_edges(self.coords)
+        for i, j, d in self.euclidean_answers:
+            self.assertEqual(edges[self.coords[i], self.coords[j]].distance, d)
+            self.assertEqual(edges[self.coords[j], self.coords[i]].distance, d)
+    
             
 if __name__ == '__main__':
     unittest.main()
