75c8b1509d4e1faebb1ba67a696993b3add4e99b
Robert Grant <rgrant@mdi.us.com>
2014-05-12 13:20:05 -0400

    First unit test created

--- pants/test/test_world.py
+++ pants/test/test_world.py
@@ -1,4 +1,24 @@
-def somefunction():
-	return None
+from ..world import World, Edge
+import unittest
 
-
+class WorldTest(unittest.TestCase):
+    known_distances = [
+        ((( 0,  0), ( 0,  1)), 1),
+        ((( 0,  0), ( 1,  0)), 1),
+        ((( 0,  1), ( 0,  0)), 1),
+        ((( 1,  0), ( 0,  0)), 1),
+        ((( 0,  0), ( 0, -1)), 1),
+        ((( 0,  0), (-1,  0)), 1),
+        ((( 0, -1), ( 0,  0)), 1),
+        (((-1,  0), ( 0,  0)), 1),
+        ((( 0,  0), ( 3,  4)), 5),
+    ]
+    
+    def test_edge_distance(self):
+        for (a, b), d in self.known_distances:
+            e = Edge(a, b)
+            self.assertEqual(e.distance, d)
+            
+            
+if __name__ == '__main__':
+    unittest.main()
