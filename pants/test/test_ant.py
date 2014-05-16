fcc4da8eb3e495f55cf1edcc4ac4e8054fadd8af
Robert Grant <rgrant@mdi.us.com>
2014-05-16 09:58:22 -0400

    Fleshed out some more unit tests

--- pants/test/test_ant.py
+++ pants/test/test_ant.py
@@ -1,57 +1,118 @@
 from ..ant import Ant
+from ..world import World
 import unittest
-from unittest import mock
 
 class AntTest(unittest.TestCase):
-	def setUp(self):
-		# TODO: Mock the world object
-		coords = [(0, 0), (1, 1), (0, 2)]
-		self.world = World(coords)
-		
-	def test_uid_behavior(self):
-		n = 5
-		base_uid = Ant(self.world).uid
-		upper_uid = base_uid + 5
-		expected_uids = range(base_uid, upper_uid)
-		uids = [Ant(world).uid for i in range(n)]
-		self.assertEqual(expected_uids, uids)
+    def setUp(self):
+        # TODO: Mock the world object
+        self.coords = [(0, 0), (1, 1), (0, 2)]
+        self.world = World(self.coords)
+        
+    def test_uid_behavior(self):
+        n = 5
+        base_uid = Ant(self.world).uid + 1
+        expected_uids = list(range(base_uid, base_uid + n))
+        uids = [Ant(self.world).uid for i in range(n)]
+        self.assertEqual(expected_uids, uids)
 
-	def test_initial_properties(self):
-		pass
+    def test_initial_properties(self):
+        ant = Ant(self.world)
+        self.assertFalse(ant.trip_complete)
+        self.assertEqual(ant.distance, 0)
+        self.assertEqual(len(ant.moves), 0)
 
-	def test_moves(self):
-		pass
+    def test_moves(self):
+        # TODO: Add case where len(ant.path) == 1
+        ant = Ant(self.world)
+        ant.path = self.world.coords[:2]
+        expected_moves = [
+            (self.world.coords[0], self.world.coords[1]),
+            (self.world.coords[1], self.world.coords[0])
+        ]
+        self.assertEqual(ant.moves, expected_moves)
+        
+    def test_clone_equality(self):
+        # TODO: mock world.distance
+        # TODO: mock ant.get_possible_moves
+        # TODO: mock ant.choose_move
+        ant = Ant(self.world, start=self.world.coords[0])
+        ant.move()
+        clone = ant.clone()
+        self.assertEqual(ant.uid, clone.uid)
+        self.assertEqual(ant.distance, clone.distance)
+        self.assertEqual(ant.path, clone.path)
+        self.assertEqual(ant.trip_complete, clone.trip_complete)
+        self.assertEqual(ant.node, clone.node)
+        self.assertEqual(ant.start, clone.start)
+        
+    def test_distance_ordering(self):
+        ant1 = Ant(self.world)
+        ant2 = Ant(self.world)
+        ant1.distance = 1
+        ant2.distance = 2
+        self.assertTrue(ant1 < ant2)
 
-	def test_clone_equality(self):
-		pass
+    def test_reset(self):
+        ant = Ant(self.world)
+        start = ant.start = (10, 10)
+        node = ant.node = (20, 20)
+        distance = ant.distance = 30
+        path = ant.path = [start, node]
+        ant.reset()
+        self.assertNotEqual(ant.start, start)
+        self.assertNotEqual(ant.node, node)
+        self.assertNotEqual(ant.distance, distance)
+        self.assertNotEqual(ant.path, path)
 
-	def test_distance_ordering(self):
-		pass
+    def test_can_move(self):
+        ant = Ant(self.world)
+        self.assertTrue(ant.can_move())
+        ant = Ant(World(list()))
+        self.assertFalse(ant.can_move())
+        # TODO: mock ant.get_possible_moves, then call:
+        #self.assertFalse(ant.can_move())
 
-	def test_reset(self):
-		pass
+    def test_get_possible_moves(self):
+        ant = Ant(self.world)
+        self.assertEqual(list(ant.get_possible_moves()), self.world.coords)
+        ant = Ant(World(list()))
+        self.assertEqual(list(ant.get_possible_moves()), [])
 
-	def test_can_move(self):
-		pass
+    def test_choose_move(self):
+        # TODO: case where no moves exist
+        # TODO: case where only one move exists
+        # TODO: case where many moves exist
+        pass
 
-	def test_get_possible_moves(self):
-		pass
+    def test_calculate_weight(self):
+        ant = Ant(self.world, alpha=2, beta=3)
+        self.assertEqual(ant.calculate_weight(1, 1), 1)
+        self.assertEqual(ant.calculate_weight(2, 1), 8)
+        self.assertEqual(ant.calculate_weight(1, 2), 4)
 
-	def test_choose_move(self):
-		pass
+    def test_make_move(self):
+        # TODO: mock world.distance
+        edge = self.world.edges[self.coords[0], self.coords[1]]
+        ant = Ant(self.world, start=edge.start)
+        path_length = len(ant.path)
+        ant.make_move(edge.end)
+        self.assertEqual(ant.distance, edge.distance)
+        self.assertEqual(len(ant.path), path_length + 1)
+        self.assertEqual(ant.path[-2], edge.start)
+        self.assertEqual(ant.path[-1], edge.end)
+        self.assertEqual(ant.node, edge.end)
 
-	def test_calculate_weight(self):
-		pass
-
-	def test_make_move(self):
-		pass
-
-	def test_move(self):
-		pass
-
-	def tearDown(self):
-		del self.world
+    def test_move(self):
+        ant = Ant(self.world)
+        # TODO: case where there are no moves to make
+        # TODO: case where there is a single move to make
+        # TODO: case where there are many moves to make
+        # TODO: case where move to make is the last move possible
+        
+    def tearDown(self):
+        del self.world
+        del self.coords
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- pants/test/test_world.py
+++ pants/test/test_world.py
@@ -2,61 +2,110 @@ from ..world import World, Edge
 import unittest
 import math
 
-class WorldTest(unittest.TestCase):
-    known_distances = [
-        (( 0,  0), ( 0,  1), 1),
-        (( 0,  0), ( 1,  0), 1),
-        (( 0,  1), ( 0,  0), 1),
-        (( 1,  0), ( 0,  0), 1),
-        (( 0,  0), ( 0, -1), 1),
-        (( 0,  0), (-1,  0), 1),
-        (( 0, -1), ( 0,  0), 1),
-        ((-1,  0), ( 0,  0), 1),
-        (( 0,  0), ( 3,  4), 5),
-    ]
-    
-    coords = [
-        (0, 0),
-        (0, 1),
-        (0, 2),
-        (1, 0),
-    ]
+class EdgeTest(unittest.TestCase):
+    def setUp(self):
+        self.edges = [
+            (( 0,  0), ( 0,  1)),
+            (( 0,  0), ( 1,  0)),
+            (( 0,  1), ( 0,  0)),
+            (( 1,  0), ( 0,  0)),
+            (( 0,  0), ( 0, -1)),
+            (( 0,  0), (-1,  0)),
+            (( 0, -1), ( 0,  0)),
+            ((-1,  0), ( 0,  0)),
+            (( 0,  0), ( 3,  4)),
+        ]
+        self.distances = [1, 1, 1, 1, 1, 1, 1, 1, 5]
 
-    euclidean_answers = [
-        (0, 1, 1),
-        (0, 2, 2),
-        (0, 3, 1),
-        (1, 2, 1),
-        (1, 3, math.sqrt(2)),
-        (2, 3, math.sqrt(5))
-    ]
+    def test_static_edge_distance(self):
+        for (c1, c2), d in zip(self.edges, self.distances):
+            self.assertEqual(Edge.distance(c1, c2), d)
+
+    def test_default_edge_distance(self):
+        edges = [Edge(a, b) for a, b in self.edges]
+        for i, e in enumerate(edges):
+            self.assertEqual(e.distance, self.distances[i])
+            
+    def test_default_pheromone_equality(self):
+        edges = [Edge(a, b) for a, b in self.edges]
+        for e in edges[1:]:
+            self.assertEqual(e.pheromone, edges[0].pheromone)
+            
+    def tearDown(self):
+        del self.edges
+        del self.distances
+
+
+class WorldTest(unittest.TestCase):
+    def setUp(self):
+        self.coords = [
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+        ]
+
+        # TODO: mock (static) Edge.distance
+        self.edges = [
+            Edge(a, b) for a in self.coords for b in self.coords
+        ]
         
-    def test_edge_distance(self):
-        for i, j, d in self.known_distances:
-            self.assertEqual(Edge.distance(i, j), d)
+        self.world = World(self.coords, self.edges)        
+        
+        self.cases = [
+            (0, 0, 0),
+            (0, 1, 1),
+            (0, 2, 2),
+            (0, 3, 1),
+            (1, 2, 1),
+            (1, 3, math.sqrt(2)),
+            (2, 3, math.sqrt(5))
+        ]
+        
+        self.no_edge_case = [
+            (-1, -1), (0, 0)
+        ]
     
+    def test_world_distance(self):
+        for i, j, d in self.cases:
+            a, b = self.coords[i], self.coords[j]
+            self.assertEqual(self.world.distance(a, b), d)
+            
+    def test_world_distance_no_edge(self):
+        self.assertEqual(self.world.distance(*self.no_edge_case), -1)
+
+    def test_world_scent_no_edge(self):
+        self.assertEqual(self.world.scent(*self.no_edge_case), 0)
+
+    def tearDown(self):
+        del self.no_edge_case
+        del self.cases
+        del self.world
+        del self.edges
+        del self.coords
+
+
+class EuclideanWorldTest(unittest.TestCase):
+    def setUp(self):
+        self.coords = [
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+        ]
+        self.edges = [Edge(a, b) for a in self.coords for b in self.coords]
+        
     def test_world_euclidean_edges(self):
         edges = World.euclidean_edges(self.coords)
-        for i, j, d in self.euclidean_answers:
-            self.assertEqual(edges[self.coords[i], self.coords[j]].distance, d)
-            self.assertEqual(edges[self.coords[j], self.coords[i]].distance, d)
+        self.assertEqual(self.edges, edges)
+        #for i, j, d in self.euclidean_answers:
+        #    self.assertEqual(edges[self.coords[i], self.coords[j]].distance, d)
+        #    self.assertEqual(edges[self.coords[j], self.coords[i]].distance, d)
+    
+    def tearDown(self):
+        del self.coords
+        del self.edges
 
-    def test_world_edge_distance(self):
-        co = (0, 0), (0, 3)
-        edges = {
-            co: Edge(*co)
-        }
-        world = World(co, edges)
-        self.assertEqual(world.distance(*co), 3)
-
-    def test_world_nonexistent_edge(self):
-        co = (0, 0), (0, 3)
-        edges = {
-            co: Edge(*co)
-        }
-        world = World(co, edges)
-        self.assertEqual(world.distance((0, 0), (0, 4)), -1)
-        self.assertEqual(world.scent((0, 0), (0, 4)), 0)
             
 if __name__ == '__main__':
     unittest.main()
