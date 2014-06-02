6b9fd4b1f6484c5a5f4be6c789c6616f0c0ddd86
Robert Grant <rgrant@mdi.us.com>
2014-06-02 09:55:39 -0400

    Added sphinxy module comments

--- pants/__init__.py
+++ pants/__init__.py
@@ -1,3 +1,9 @@
+"""A Python3 implementation of the Ant Colony Optimization Meta-Heuristic.
+
+.. moduleauthor:: Robert Grant <rhgrant10@gmail.com>
+
+"""
+
 from .ant import Ant 
 from .world import World, Edge, Node
 from .solver import Solver
