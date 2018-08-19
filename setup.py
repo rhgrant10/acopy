6151671609ed19d5968e774e6afa06bffa1b8a8c
Robert Grant <rhgrant10@gmail.com>
2018-08-18 19:44:21 -0700

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rhgrant10/acopy',
-    version='0.5.2',
+    version='0.6.0',
     zip_safe=True,
 )
